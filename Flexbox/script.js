4ebd473c7e5570a617f067e82c9a222cb4971a60
Rifqa Nabila Muti <145187544+RifqaNabilaMuti@users.noreply.github.com>
2023-10-05 15:04:01 +0700

    Add files via upload

---
+++ Flexbox/script.js
@@ -0,0 +1,35 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle ('slide');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
